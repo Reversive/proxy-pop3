1198a10d25303edc5a5d74df2872035f3a7a00bc
Matias Enrique Pavan <matiaspavan@gmail.com>
2021-10-24 00:47:31 -0300

    Deleted unused proxy folder

--- proxy/include/pop3_handler.h
+++
@@ -1,7 +0,0 @@
-#ifndef PROXY_POP3_HANDLER_H
-#define PROXY_POP3_HANDLER_H
-#include "selector.h"
-
-void accept_pop3_connection(struct selector_key * key);
-
-#endif //PROXY_POP3_HANDLER_H
--- proxy/pop3_handler.c
+++
@@ -1,5 +0,0 @@
-#include "include/pop3_handler.h"
-
-void accept_pop3_connection(struct selector_key * key) {
-    // TODO: implement accept pop3 connection
-}
