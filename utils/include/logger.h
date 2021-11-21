4dab9e6c7aa1f3869f803aafaa2d8db2591e962e
Reversive <matiaspavan@gmail.com>
2021-11-21 16:36:51 -0300

    Added logging formatting
    Co-authored-by: patrickmdey <52257842+patrickmdey@users.noreply.github.com>
    Co-authored-by: matias <mflombardi2000@gmail.com>
    Co-authored-by: Santos Rosati <srosati@itba.edu.ar>

--- utils/include/logger.h
+++ utils/include/logger.h
@@ -2,6 +2,7 @@
 #define __logger_h_
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* 
 *  Macros y funciones simples para log de errores.
@@ -16,14 +17,20 @@ extern LOG_LEVEL current_level;
 /**
 *  Minimo nivel de log a registrar. Cualquier llamada a log con un nivel mayor a newLevel sera ignorada
 **/
-void setLogLevel(LOG_LEVEL newLevel);
+void set_log_level(LOG_LEVEL newLevel);
 
-char * levelDescription(LOG_LEVEL level);
+char * level_description(LOG_LEVEL level);
+
+char * get_current_timestamp();
 
 // Debe ser una macro para poder obtener nombre y linea de archivo. 
 #define log(level, fmt, ...)   {if(level >= current_level) {\
 	FILE * toPrint = ( level >= ERROR ) ? stderr : stdout; \
-	fprintf(toPrint, "%s: %s:%d, ", levelDescription(level), __FILE__, __LINE__); \
+	\
+	if(level <= INFO) \
+		fprintf(toPrint, "%s[%s][ %s ]\033[0m\t%s:%d, ", "\033[0;36m", get_current_timestamp(), level_description(level), __FILE__, __LINE__); \
+	else \
+		fprintf(toPrint, "%s[%s][ %s ]\033[0m\t%s:%d, ", "\033[0;31m", get_current_timestamp(), level_description(level), __FILE__, __LINE__); \
 	fprintf(toPrint, fmt, ##__VA_ARGS__); \
 	fprintf(toPrint,"\n"); }\
 	if ( level==FATAL) exit(1);}
--- utils/logger.c
+++ utils/logger.c
@@ -2,13 +2,20 @@
 
 LOG_LEVEL current_level = DEBUG;
 
+char * get_current_timestamp() {
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    static char buffer[20];
+    sprintf(buffer, "%d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    return buffer;
+}
 
-void setLogLevel(LOG_LEVEL newLevel) {
+void set_log_level(LOG_LEVEL newLevel) {
 	if ( newLevel >= DEBUG && newLevel <= FATAL )
 	   current_level = newLevel;
 }
 
-char * levelDescription(LOG_LEVEL level) {
+char * level_description(LOG_LEVEL level) {
     static char * description[] = {"DEBUG", "INFO", "ERROR", "FATAL"};
     if (level < DEBUG || level > FATAL)
         return "";
